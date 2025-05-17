973d18728374f5469fbaa0fbc8651597e985f339
Parth Sharma <engineerparthh>
2025-05-17 16:19:18 +0530

    update

--- datatype/BasicCode.java
+++ datatype/BasicCode.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class BasicCode {
     public static void main(String args[]) {
-        int a = 8;
-        int b = 78;
-        System.out.println((a != b)); // prints true because 43 is not equal to 34
+        int a = 23;
+        int b = 2;
+        System.out.println((a != b)); 
     }
 }
