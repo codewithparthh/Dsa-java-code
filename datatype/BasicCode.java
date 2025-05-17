5d71d031d0bd364ed7cf5c3466da2708d8e66c53
Parth Sharma <engineerparthh>
2025-05-17 15:16:28 +0530

    joyo

---
+++ datatype/BasicCode.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BasicCode {
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "sharma ji";
+        System.out.println(name);
+        a=b;
+        System.out.println(a);
+    }
+}
