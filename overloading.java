cd1b3fb78741320c75aef678b76ca1155fa454c4
Parth Sharma <engineerparthh>
2025-12-07 23:19:54 +0530

    D6

--- overloading.java
+++ overloading.java
@@ -10,6 +10,6 @@ public class overloading {
 
     public static void main(String args[]) {
         System.out.println(sum(3, 5));
-        System.out.println(sum(3, 5, 9));
+        System.out.println(sum(3, 5, 8));
     }
 }
