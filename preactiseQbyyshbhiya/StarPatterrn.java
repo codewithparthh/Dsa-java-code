dc04c2a2807f68328e24cbd780d4bfbe8e799b9f
Parth Sharma <engineerparthh>
2026-01-13 23:47:28 +0530

    D12

--- preactiseQbyyshbhiya/StarPatterrn.java
+++ preactiseQbyyshbhiya/StarPatterrn.java
@@ -2,7 +2,7 @@ public class StarPatterrn {
     public static void main(String args[]) {
 
         for(int line = 1; line <= 5; line++) {
-            System.out.println("**/n*");
+            System.out.println("***");
         }
     }
 }
