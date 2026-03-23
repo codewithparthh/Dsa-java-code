11e8774fe78ce33957ec25f7e8320a1b15882f65
Parth Sharma <engineerparthh>
2026-03-23 21:11:59 +0530

    array

---
+++ Largestarray.java
@@ -0,0 +1,26 @@
+import java .util.*;
+
+public class Largestarray {
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i=0; i<numbers.length; i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("smallest value is : "+ smallest);
+        return largest;
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {-0,0,};
+        System.out.println("largest value is : " + getLargest(numbers));
+
+    }
+}
