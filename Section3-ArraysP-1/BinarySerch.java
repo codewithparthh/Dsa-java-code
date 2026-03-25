25b8c04f59427136b85225aebb6d14fe27dbfe91
Parth Sharma <engineerparthh>
2026-03-25 14:26:24 +0530

    binary

---
+++ Section3-ArraysP-1/BinarySerch.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BinarySerch {
+    public static int binarySearch(int numbers[], int key){
+        int start = 0, end = numbers.length-1;
+
+        while (start <= end){
+            int mid = (start + end)/2;
+
+            if (numbers[mid] == key){
+                return  mid;
+               }
+               else if(numbers[mid] < key){
+                    start = mid+1;
+                }
+                else {
+                    end = mid-1;
+                }
+            }
+            return -1;
+        }
+        public static void main (String args[]) {
+            int numbers [] = {2,4,6,8,10,12,14};
+            int key = 14;
+
+            int result = binarySearch(numbers, key);
+
+        System.out.println("index for keys is : " + result);
+        }
+    }
+
+
+
+
