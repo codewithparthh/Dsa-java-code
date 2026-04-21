e450480acd642b24118c9fa0601f82f2318cc497
Parth Sharma <engineerparthh>
2026-04-21 23:12:12 +0530

    bubble sort

---
+++ Section5-/Bubblesort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Bubblesort{
+
+    public static void Bubblesort(int arr[]){
+        for(int turn=0; turn<arr.length-1; turn++){
+            for(int j=0; j<arr.length-1-turn; j++){
+                if(arr[j] > arr[j+1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main (String args[]){
+        int arr[] = {3,5,1,8,9,6};
+
+        Bubblesort(arr); // function call ✅
+
+        // print sorted array
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
