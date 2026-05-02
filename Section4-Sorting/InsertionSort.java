9be5e101b42a9205737192b22ca3b7491cd19b78
Parth Sharma <parthsharma7979@gmail.com>
2026-05-02 17:04:35 +0530

    sorting

---
+++ Section4-Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class InsertionSort{
+    public static void InsertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j] )
+{
+            arr[j+1] = arr[j];
+            j--;
+
+}
+            arr[j+1] = current;
+         }
+    }
+    public static void main (String args[])
+{
+    int arr[] = {5,3,1,4,3,2};
+    InsertionSort(arr);
+    for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+    }
+  }
+}
