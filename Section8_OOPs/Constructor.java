d4975cf4220ceb8316f73ad12a0372fc08cb2753
Parth Sharma <parthsharma7979@gmail.com>
2026-09-12 18:03:35 +0530

    pushed

---
+++ Section8_OOPs/Constructor.java
@@ -0,0 +1,20 @@
+package Section8_OOPs;
+
+public class Constructor {
+
+    public static void main(String args[]) {
+
+        Student s1 = new Student();
+
+    }
+}
+
+class Student {
+
+    String name;
+    int roll;
+
+    Student() {
+        System.out.println("Constructor called");
+    }
+}
